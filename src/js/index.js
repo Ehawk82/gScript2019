e0aa57229d79e4d3d8ffda9521409e4cd51a13db
Jason Graziano <mrehawk82@gmail.com>
2019-01-14 20:34:35 -0500

    added functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,9 @@
 function bySel(x) { return document.querySelector(x) };
 function byId(x) { return document.getElementById(x) };
+function byClass(x) { return document.getElementsByClassName(x) };
+function bySelAll(x) { return document.querySelectorAll(x) };
+function deleteThis(x) { return x.remove() };
+function makeFull(x) {return x.className = x + "_full" };
+function takeFull(x) { var y,z; return y = x.className.split('_full'), z = y[0], x.className = z };
 
 module.exports = { bySel: bySel, byElementId: byId };
