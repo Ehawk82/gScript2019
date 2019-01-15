a3d1d31cb678993de06d30a6c2167d9524358c7d
Jason Graziano <mrehawk82@gmail.com>
2019-01-14 20:46:17 -0500

    building a library

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# gScript2019
+# gScript2019 
+____
+
+a library specified to the code habits of Ehawk LLC
+
+____
+
+Features:
+
+- creating and removing nodes.
+- toggle class name "full" <sup>1</sup>
+
+notes:
+
+<sup>1</sup> takeFull and makeFull will add/remove the string ""_" _full_" on the class the className.  CSS transitions are required to custimize effects
--- src/js/index.js
+++ src/js/index.js
@@ -5,5 +5,7 @@ function bySelAll(x) { return document.querySelectorAll(x) };
 function deleteThis(x) { return x.remove() };
 function makeFull(x) {return x.className = x + "_full" };
 function takeFull(x) { var y,z; return y = x.className.split('_full'), z = y[0], x.className = z };
+function createEle(x) { return document.createElement(x) };
+function byTag(x, y) { if (!y || y === null) {y = 0}  return docuSment.getElementsByTagName(x)[y] };
 
-module.exports = { bySel: bySel, byElementId: byId };
+module.exports = { bySel: bySel, byId: byId, byClass: byClass, bySelAll: bySelAll, deleteThis: deleteThis, makeFull: makeFull, takeFull: takeFull };
