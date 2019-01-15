4cc31b1f83df855b9b63a119073107436d85dc76
Jason Graziano <mrehawk82@gmail.com>
2019-01-14 20:25:13 -0500

    making things

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,4 @@
-console.log("init");
+function bySel(x) { return document.querySelector(x) };
+function byId(x) { return document.getElementById(x) };
+
+module.exports = { bySel: bySel, byElementId: byId };
