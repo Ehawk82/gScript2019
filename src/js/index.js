5749908c2fdc64751ecc5a2ccbdaf8f738ade1ac
Jason Graziano <mrehawk82@gmail.com>
2019-01-24 14:12:45 -0500

    error fix

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ function deleteThis(x) { return x.remove() };
 function makeFull(x) {return x.className = x.className + "_full" };
 function takeFull(x) { var y,z; return y = x.className.split('_full'), z = y[0], x.className = z };
 function createEle(x) { return document.createElement(x) };
-function byTag(x, y) { if (!y || y === null) {y = 0}  return docuSment.getElementsByTagName(x)[y] };
+function byTag(x, y) { if (!y || y === null) {y = 0}  return document.getElementsByTagName(x)[y] };
 function saveLS(x, y) { return localStorage.saveItem(x, JSON.stringify(y)) };
 function loadLS(x) { return localStorage.getItem(x) };
 function clearLS() { return localStorage.clear() };
