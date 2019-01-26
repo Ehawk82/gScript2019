0d851ea132dc9eb2fbe9c80f66592d79b6c25d14
Jason Graziano <mrehawk82@gmail.com>
2019-01-26 11:35:34 -0500

    change order

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+/* standard returns */
 function bySel(x) { return document.querySelector(x) };
 function byId(x) { return document.getElementById(x) };
 function byClass(x) { return document.getElementsByClassName(x) };
@@ -7,9 +8,11 @@ function makeFull(x) {return x.className = x.className + "_full" };
 function takeFull(x) { var y,z; return y = x.className.split('_full'), z = y[0], x.className = z };
 function createEle(x) { return document.createElement(x) };
 function byTag(x, y) { if (!y || y === null) {y = 0}  return document.getElementsByTagName(x)[y] };
+function makeLock(x) { return x.className = x.className + "_locked" };
+function takeLock(x) { var y, z; return y = x.className.split("_locked"), z = y[0], x.className = z };
+
+/* localStorage returns */
 function saveLS(x, y) { return localStorage.saveItem(x, JSON.stringify(y)) };
 function loadLS(x) { return localStorage.getItem(x) };
 function clearLS() { return localStorage.clear() };
-function removeLSitem(x) { return localStorage.removeItem(x) };
-function makeLock (x) { return x.className = x.className + "_locked" };
-function takeLock (x) { var y, z; return y = x.className.split("_locked"), z = y[0], x.className = z };
+function removeLSitem(x) { return localStorage.removeItem(x) };/*untested*/
