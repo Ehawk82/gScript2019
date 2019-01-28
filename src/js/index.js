e5a19d2e55817b5e2b1d4d3d1a8c0f66b24c2185
Jason Graziano <mrehawk82@gmail.com>
2019-01-28 09:34:16 -0500

    organizing

--- src/js/index.js
+++ src/js/index.js
@@ -13,6 +13,7 @@ function takeLock(x) { var y, z; return y = x.className.split("_locked"), z = y[
 
 /* localStorage returns */
 var ts = Math.round(new Date().getTime()/1000);
+
 var basicUser = {
     health: 100,
     manna: 5,
@@ -28,29 +29,55 @@ var basicUser = {
 
 };
 
+var midUser = {
+    health: 100,
+    manna: 15,
+    life: 2,
+    power: 2,
+    speed: 2,
+    acc: 2,
+    intel: 2,
+    defense: 21,
+    gBool: false,
+    tStamp: ts,
+    level: 2
+
+};
+
+var hiUser = {
+    health: 100,
+    manna: 20,
+    life: 4,
+    power: 4,
+    speed: 4,
+    acc: 4,
+    intel: 4,
+    defense: 44,
+    gBool: false,
+    tStamp: ts,
+    level: 4
+
+};
+
 function saveLS(x, y) { return localStorage.setItem(x, JSON.stringify(y)) };
 function loadLS(x) { return localStorage.getItem(x) };
+function parseLS(x) { var y = loadLS(x), z = JSON.parse(y); return z };
 function clearLS() { return localStorage.clear() };
-function removeLSitem(x) { return localStorage.removeItem(x) };/*untested*/
+function removeLSitem(x) { return localStorage.removeItem(x) };
 function LSinit(x,y) {
-	clearLS();
 	var keyname = localStorage.getItem(x);
 	if (!keyname || keyname === null) {
 		var p;
 		if (y === "basicUser") {
             p = basicUser;
-		} else {
+		} else if (y === "midUser") {
+            p = midUser;
+		} 
+        else if (y === "hiUser") {
+            p = hiUser;
+        } else {
             p = y;
 		}
 	    localStorage.setItem(x, JSON.stringify(p));
 	};
-
-	var z = localStorage.getItem(keyname);
-
-	if (z) {
-
-		var a = JSON.parse(z);
-	}; 
-
-	return console.log(z), console.log(a);
 };
